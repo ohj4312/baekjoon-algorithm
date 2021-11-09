8d90f5793271e9b1be6059fc3ad2cc24a5a79f66
ohj4312 <ohj4312@naver.com>
2021-11-09 21:45:04 +0900

    bj 9095 - 1,2,3 더하기 완성 21/11/09

--- Baekjoon/src/BruteForce/recursive/BJ9095.java
+++ Baekjoon/src/BruteForce/recursive/BJ9095.java
@@ -4,37 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class BJ9095 {
-	private static int cnt=0,sum=0,n;
-	private static int[] arr;
+	private static int cnt=0,n;
+	private static int[] arr=new int[] {1,2,3},ans;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		n=Integer.parseInt(br.readLine());
-		
-		arr=new int[n];
-		
-		for(int i=1;i<n;i++) {
-			arr[i]=i;
+		int tc=Integer.parseInt(br.readLine());
+		for(int t=0;t<tc;t++) {
+			n=Integer.parseInt(br.readLine());
+			ans=new int[n];
+			rec(0);
+			
+			System.out.println(cnt);
+			cnt=0;
 		}
-		
-		rec();
-		
-		System.out.println(cnt);
-		
 	}
-	private static void rec() {
+	private static void rec(int sum) {
 		if(sum>=n) {
-			if(sum==n) cnt++;
+			if(sum==n) {
+				cnt++;
+			}
 			sum=0;
 			return;
 		}
 		
 		
-		for(int i=1;i<n;i++) {
-			sum+=arr[i];
-			System.out.println(sum);
-			rec();
+		for(int i=0;i<3;i++) {
+			int tempsum=sum;
+			tempsum+=arr[i];
+			rec(tempsum);
+			
 		}
-		
 	}
 }
