751c14ffa3bcd372fffa77b896b8e006aec00112
ohj4312 <ohj4312@naver.com>
2022-01-08 23:29:08 +0900

    재귀를 이용하는 방법

--- Baekjoon/src/BruteForce/recursive/BJ14501.java
+++ Baekjoon/src/BruteForce/recursive/BJ14501.java
@@ -6,14 +6,16 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BJ14501 {
+	
+	private static int n,max=0,sum=0;
+	private static int[][] tp;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int n=Integer.parseInt(br.readLine());
+		n=Integer.parseInt(br.readLine());
 		StringTokenizer st=null;
 		
-		int[][] tp=new int[n+1][2]; //입력배열
-		int[][] dp=new int[n+1][n+1]; //동적테이블 
-		boolean[] ck=new boolean[n+1]; //상담이 잡힌날 제외
+		tp=new int[n+1][2]; //입력배열
 
 		for(int t=1;t<=n;t++) {
 			st=new StringTokenizer(br.readLine()," ");
@@ -21,37 +23,30 @@ public class BJ14501 {
 			tp[t][1]=Integer.parseInt(st.nextToken());
 		}
 		
+		solve(1);
 		
-		/*		for(int i=1;i<=n;i++) {
-					for(int j=1;j<=n;j++) {
-						//||j+tp[i][0]>n
-						if(j-tp[i][0]<0||i+tp[i][0]>n) dp[i][j]=dp[i-1][j]; //일수가 안되면 패쓰
-						else {
-							dp[i][j]=Math.max(dp[i-1][j], dp[i-1][j-tp[i][0]]+tp[i][1]); //점화식
-							if(dp[i][j]!=dp[i-1][j]) {
-								
-							}
-						}
-					}
-				}*/
+		System.out.println(max);
 		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n;j++) {
-				//||j+tp[i][0]>n
-				if(i-tp[j][0]<0||j+tp[i][0]>n) dp[j][i]=dp[j-1][i]; //일수가 안되면 패쓰
-				else {
-					dp[j][i]=Math.max(dp[j-1][i], dp[j-1][i-tp[j][0]]+tp[j][1]); //점화식
-				
-				}
-			}
+	}
+
+	private static void solve(int s) {
+		//퇴사일 이면 끝
+		if(s>=n) {
+			if(sum>max) max=sum;
+			System.out.println(s+"퇴사일"+sum+" max:"+max);
+			return;
 		}
 		
-		for(int i=0;i<=n;i++) {
-			for(int j=0;j<=n;j++) {
-				System.out.print(dp[i][j]+" ");
+		for(int i=s;i<n;i++) {
+			if(i+tp[i][0]>n) {
+				if(sum>max) max=sum;
+				System.out.println("sum : "+sum);
+				continue;
 			}
-			System.out.println();
+			sum+=tp[i][1];
+			System.out.println(s+" checke : "+sum+" , "+i+" 몇을 더하냐면? "+tp[i][0]);
+			solve(i+tp[i][0]);
+			sum-=tp[i][1];
 		}
-		System.out.println(dp[n][n]);
 	}
 }
